0301474dc9f0cd463d53b86b739186533b5098ce
Mike Fitzpatrick <mlfitz2@gmail.com>
2022-02-06 19:44:19 -0800

    cities save to list, clicking cities on list pulls new search

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-
+//Displays current weather conditions
 async function searchWeather(cityName) {
 
     //Current weather
@@ -7,7 +7,7 @@ async function searchWeather(cityName) {
     let response = await fetch(apiUrl);
     let res = await response.json();
 
-
+    //Builds elements for data
     let cardArea = document.getElementById('current-weather-area');
     let tempEl = document.createElement('p');
     let humidityEl = document.createElement('p');
@@ -15,48 +15,27 @@ async function searchWeather(cityName) {
     let nameEl = document.createElement('h3');
     let imgEl = document.createElement('img');
 
-
+    //Fills in data from API
     nameEl.textContent = `${res.name}`;
     tempEl.textContent = `Temp: ${res.main.temp} °F`;
     humidityEl.textContent = `Humidity: ${res.main.humidity}%`;
     windSpeedEl.textContent = `Wind Speed: ${res.wind.speed} mph`;
-
-
-    
     imgEl.src = `https://openweathermap.org/img/w/${res.weather[0].icon}.png`
     
+    //Displays data
     nameEl.append(imgEl);
     cardArea.append(nameEl, tempEl, humidityEl, windSpeedEl);
 }
 
-
+//5-day forecast
 async function searchForecast(cityName) {
     let forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=979a65fd5e03b76d473b58f3098b3af0&units=imperial`;
     let response = await fetch(forecastUrl);
     let res = await response.json();
 
+    //FCreate elements for forecast data
 
-    let thisHour = moment().format("h a");
-    let x = '';
-    if (thisHour > '09 pm') {
-        x = 1;
-    } else if (thisHour <= '09 pm' && thisHour > '06 pm') {
-        x = 2;
-    } else if (thisHour <= '06 pm' && thisHour > '03 pm') {
-        x = 3;
-    } else if (thisHour <= '03 pm' && thisHour > '12 pm') {
-        x = 4;
-    } else if (thisHour <= '12 pm' && thisHour > '09 am') {
-        x = 5;
-    } else if (thisHour <= '09 am' && thisHour > '06 am') {
-        x = 6;
-    } else if (thisHour <= '06 am' && thisHour > '03 am') {
-        x = 7;
-    } else {
-        x = 8;
-    }
-    console.log(thisHour);    
-    for ( i = x; i < 40; i+=8) {
+    for ( i = 0; i < 40; i+=8) { //forecast every 3 hours, so get every 8 forecasts to be once a day
         let forecastArea = document.getElementById('forecast-area');
         let forecastCard = document.createElement('div');
         forecastCard.setAttribute('class', 'forecast-card');
@@ -65,35 +44,64 @@ async function searchForecast(cityName) {
         let windSpeedEl = document.createElement('p');
         let nameEl = document.createElement('h3');
         let imgEl = document.createElement('img');
-        let dateEl = document.createElement('p');
     
+        //Add data from API
         nameEl.textContent = `${res.city.name}`;
         tempEl.textContent = `Temp: ${res.list[i].main.temp} °F`;
         humidityEl.textContent = `Humidity: ${res.list[i].main.humidity}%`;
         windSpeedEl.textContent = `Wind Speed: ${res.list[i].wind.speed} mph`;
-        dateEl.textContent = `Date: ${res.list[i].dt_txt}`;
-
         imgEl.src = `https://openweathermap.org/img/w/${res.list[i].weather[0].icon}.png`
-        console.log(res.list[0].weather[0].icon)
 
-
-        dateEl.append(imgEl);
-    
-        forecastCard.append(dateEl, tempEl, humidityEl, windSpeedEl);
+        //Display data
+        nameEl.append(imgEl);
+        forecastCard.append(nameEl, tempEl, humidityEl, windSpeedEl);
         forecastArea.append(forecastCard);
-
     }
+}
 
+//Save the cities that have been searched
+function saveCity() {
+    let savedCities = document.getElementById('city-input').value;
+    let oldList = JSON.parse(localStorage.getItem('savedCities'));
+    if (oldList == null) {
+        oldList = [];
+    }
+    oldList.push(savedCities);
+    localStorage.setItem('savedCities', JSON.stringify(oldList))
+}
+
+//Display cities that have been searched
+function getCityList() {
+    let savedList = JSON.parse(localStorage.getItem('savedCities'));
+    savedList.forEach(element => {
+        let newCity = document.createElement('li');
+        newCity.textContent = element.toUpperCase();
+        newCity.setAttribute('class', 'saved-city');
+        
+        let citiesList = document.getElementById('saved-cities-area');
+        citiesList.append(newCity);
+        
+        //User can click on saved city to get updated forecast
+        newCity.addEventListener('click', function(){
+            let savedCity = this.textContent;
+            console.log(savedCity);
+            searchWeather(savedCity);
+            searchForecast(savedCity);
+        })
+    })
 }
 
 
 
+
+window.addEventListener('load', getCityList())
 
 document.getElementById('search-button').addEventListener('click', function(event) {
     event.preventDefault();
     let myCity = document.getElementById('city-input');
     searchWeather(myCity.value);
     searchForecast(myCity.value);
+    saveCity(myCity.value);
 })
 
 
